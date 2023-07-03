780bd3c2c04885214701c6650fa536d4a20a5651
Pawan Shiwakoti <pawanshiwakoti45@gmail.com>
2023-07-03 12:12:17 -0500

    DTOS created and address crud done

--- aluminimanagement/src/main/java/com/miu/waa/aluminimanagement/repository/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miu.waa.aluminimanagement.repository;
-
-import com.miu.waa.aluminimanagement.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-
-}
