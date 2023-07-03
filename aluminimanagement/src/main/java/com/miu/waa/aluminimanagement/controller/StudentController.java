979685a79e8e9e6cb10fd1f27b0195a0c746dc3c
sayal-aryal <sayalaryal2@gmail.com>
2023-07-03 11:47:46 -0500

    Added: Student Crud added and data.sql added

--- aluminimanagement/src/main/java/com/miu/waa/aluminimanagement/controller/StudentController.java
+++ aluminimanagement/src/main/java/com/miu/waa/aluminimanagement/controller/StudentController.java
@@ -1,4 +1,32 @@
 package com.miu.waa.aluminimanagement.controller;
 
+import com.miu.waa.aluminimanagement.model.StudentPerState;
+import com.miu.waa.aluminimanagement.model.dto.StudentDto;
+import com.miu.waa.aluminimanagement.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/students")
+@RequiredArgsConstructor
 public class StudentController {
+    private final StudentService studentService;
+    @GetMapping
+    public List<StudentDto> findAll(
+            @RequestParam(required = false, defaultValue = "") String filter,
+            @RequestParam(required = false, defaultValue = "") String value
+    ) {
+        var students = studentService.findAll(filter, value);
+        return students;
+    }
+
+    @GetMapping("/perState")
+    public List<StudentPerState> noOfStudentsPerState(){
+        return studentService.noOfStudentsPerState();
+    }
+
+
 }
--- aluminimanagement/src/main/java/com/miu/waa/aluminimanagement/service/StudentService.java
+++ aluminimanagement/src/main/java/com/miu/waa/aluminimanagement/service/StudentService.java
@@ -1,4 +1,13 @@
 package com.miu.waa.aluminimanagement.service;
 
-public class StudentService {
+import com.miu.waa.aluminimanagement.model.StudentPerState;
+import com.miu.waa.aluminimanagement.model.dto.StudentDto;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<StudentDto> findAll(String filter, String value);
+
+    List<StudentPerState> noOfStudentsPerState();
 }
